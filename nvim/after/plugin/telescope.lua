947bb790b0c0f65b028dfd23f180001dac99461d
Taylor Mendez Bauer <Taylor.MendezBauer@garmin.com>
2024-03-13 16:24:38 -0500

    Some more fiddling, no new functionality
    Change-Id: I104f5c1c09cb796ad3063662e51b95ea000a3507

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,6 +1,10 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<c-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+	builtin.grep_string({ search = vim.fn.input("grep > ") });
 end)
+
+-- use telescope for looking up tags with "gt"
+-- from https://dev.to/oinak/neovim-config-from-scratch-part-ii-11k9
+vim.keymap.set('n', 'gt', builtin.tags, { desc = '[g]o to c[t]ags (telescope)', noremap = true })
--- nvim/lua/taylor/packer.lua
+++ nvim/lua/taylor/packer.lua
@@ -15,4 +15,5 @@ return require('packer').startup(function(use)
 
   use { "catppuccin/nvim", as = "catppuccin" }
   use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use 'christoomey/vim-tmux-navigator'
 end)
