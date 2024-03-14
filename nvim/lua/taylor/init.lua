c4533e15a437e3c268ffb055a6bfd8d78fc41f06
Taylor Mendez Bauer <taylor.mendezbauer@garmin.com>
2024-03-14 14:57:30 -0500

    Add format-on-save for .cpp
    Change-Id: I79722ef1882b286cd0c08b9e776c2efe92e8af79

--- nvim/lua/taylor/init.lua
+++ nvim/lua/taylor/init.lua
@@ -4,3 +4,16 @@ require("taylor.packer")
 -- Turn on "hybrid" line numbering
 vim.o.number = true
 vim.o.relativenumber = true
+
+vim.api.nvim_create_augroup("AutoFormat", {})
+vim.api.nvim_create_autocmd(
+    "BufWritePost",
+    {
+        pattern = "*.cpp",
+        group = "AutoFormat",
+        callback = function()
+            vim.cmd("silent !clang-format -i %")            
+            vim.cmd("edit")
+        end,
+    }
+)
